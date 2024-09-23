3f7dd614facdb14b93d4d168fd0ad4fc5be0ea8e
waleed12309 <mw7264623@gmail.com>
2024-09-23 11:12:59 +0500

    ep-1

---
+++ ep-1/index.js
@@ -0,0 +1,26 @@
+//Template literals
+
+const age =20;
+const fullname="john doe";
+
+
+
+console.log("hello mr"+fullname+"your age is: "+age);
+console.log(`
+    My name is 
+    ${fullname} 
+    and I am ${age} years old`);
+
+
+
+   const a=10;
+   const b=89;
+
+
+   
+   console.log("sum of "+a+" and "+b+" is :"+b+a);
+   console.log(`sum of ${a} and ${b} is ${a+b}`);
+
+   
+
+
